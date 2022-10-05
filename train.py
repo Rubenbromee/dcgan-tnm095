862e68ea94c621e6416d29a8f4c5dda9b1ddf937
Ruben Bromée <ruben.bromee@gmail.com>
2022-10-05 20:24:04 +0200

    chore: plots and images

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ import cv2
 import os
 import PIL
 import numpy as np
+import matplotlib.image as img
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # Determining GPU or CPU
 LEARNING_RATE = 2e-4 # How drastic the change of the model is between each epoch
@@ -23,9 +24,9 @@ LEARNING_RATE = 2e-4 # How drastic the change of the model is between each epoch
 # 2 x 64 to train gen and disc in parallell?
 BATCH_SIZE = 128 
 IMAGE_SIZE = 64
-CHANNELS_IMG = 1 # Grayscale/RGB/RGBA etc.
+CHANNELS_IMG = 3 # Grayscale/RGB/RGBA etc.
 Z_DIM = 100 # Dimension of the initial uniform distribution from the paper
-NUM_EPOCHS = 6 # Number of training cycles
+NUM_EPOCHS = 11 # Number of training cycles
 FEATURES_DISC = 64 
 FEATURES_GEN = 64
 
@@ -43,11 +44,12 @@ transforms = transforms.Compose(
 )
 
 # Data loading
-dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms,
-                       download=True)
-
-# dataset = datasets.CelebA(root="dataset/", split='train', transform=transforms,
+# dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms,
 #                        download=True)
+
+
+dataset = datasets.CelebA(root="dataset/", split='train', transform=transforms,
+                       download=True)
 loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 gen = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device) # Sending the generator to the device
@@ -58,7 +60,7 @@ initialize_weights(disc) # Initialize weights for all operations in the discrimi
 # Optimizers for the weights? Uses Adam algorithm?
 opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
 opt_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
-criterion = nn.MSELoss() # 
+criterion = nn.BCELoss() # 
 
 # To see progression
 fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device) 
@@ -117,34 +119,39 @@ for epoch in range(NUM_EPOCHS):
 				writer_fake.add_image("Fake", img_grid_fake, global_step=step)
 			step += 1
 
-			if epoch == 5:
+			if epoch == 10:
 				for i in range(32):
 					np_fake = fake.cpu().numpy()
 					np_real = real.cpu().numpy()
 
-					path_fake = os.path.join('D:\DCGAN\MSE_5_F', str(batch_idx) + '_' + str(i) + '_F' + '.jpg')
-					path_real = os.path.join('D:\DCGAN\MSE_5_R', str(batch_idx) + '_' + str(i) + '_R' + '.jpg')
+					path_fake = os.path.join('D:\DCGAN\CELEBA_BCE_10_F', str(batch_idx) + '_' + str(i) + '_F' + '.jpg')
+					path_real = os.path.join('D:\DCGAN\CELEBA_BCE_10_R', str(batch_idx) + '_' + str(i) + '_R' + '.jpg')
 
 					# # MNIST
-					cv2.imwrite(path_fake, np.multiply(np_fake[i, 0, :, :], 255).astype(np.uint8))
-					cv2.imwrite(path_real, np.multiply(np_real[i, 0, :, :], 255).astype(np.uint8))
+
+					# cv2.imwrite(path_fake, np.multiply(np_fake[i, 0, :, :], 255).astype(np.uint8))
+					# cv2.imwrite(path_real, np.multiply(np_real[i, 0, :, :], 255).astype(np.uint8))
 
 					# CELEBA
-					# img_reshape_f = np.reshape(np_fake[i, :, :, :], (64, 64, 3))
-					# img_reshape_r = np.reshape(np_real[i, :, :, :], (64, 64, 3))
-					# img_fake = PIL.Image.fromarray(img_reshape_f, 'RGB')
-					# img_real = PIL.Image.fromarray(img_reshape_r, 'RGB')
-					# img_fake.save(path_fake, 'JPEG')
-					# img_real.save(path_real, 'JPEG')
-					# cv2.imwrite(path_fake, np.multiply(img_reshape_f, 255).astype(np.uint8))
-					# cv2.imwrite(path_real, np.multiply(img_reshape_r, 255).astype(np.uint8))
+
+					# Transpose to valid image dimensions
+					np_fake_t = np.transpose(np_fake[i, :, :, :], (1, 2, 0))
+					np_real_t = np.transpose(np_real[i, :, :, :], (1, 2, 0))
+
+					# Normalize image values to [0, 1]
+					np_fake_n = (np_fake_t - np.min(np_fake_t)) / np.ptp(np_fake_t)
+					np_real_n = (np_real_t - np.min(np_real_t)) / np.ptp(np_real_t)
+
+					# Save images
+					img.imsave(path_fake, np_fake_n)
+					img.imsave(path_real, np_real_n)
 
 # Plotting
 plt.figure()
 plt.plot(loss_data_disc, label='Loss Discriminator')
 plt.plot(loss_data_gen, label='Loss Generator')
 plt.legend(loc="upper right")
-plt.title(str(NUM_EPOCHS) + ' EPOCHS, MNIST, MSE')
+plt.title(str(NUM_EPOCHS) + ' EPOCHS, CELEBA, BCE')
 plt.ylabel('Loss')
 plt.figure()
 np_grid = img_grid_fake.cpu().numpy()
